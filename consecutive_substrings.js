b24bf72024329344cd7fd387bf6e9ac51042fa49
Tryston <trystonproffit@gmail.com>
2026-02-05 14:20:35 -0500

    Psd code and js file finsihed, created tests and the main function

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,9 +1,56 @@
 function consecutiveSubstrings(string) {
-  // type your code here
+  // Create a variable that stores an array where all substrings will be stored
+  const result = [];
+  // Create the exterior loop
+  for (let i = 0; i < string.length; i++) {
+    //substrings start here
+    for (let j = i + 1; j <= string.length; j++) {
+      result.push(string.slice(i, j));
+    }
+  }
+  // return the substring array
+  return result;
 }
 
 if (require.main === module) {
   // add your own tests in here
+  console.log("Test for Repeat characters");
+  console.log("Expecting: ['t', 'tt', 'ttt', 't', 'tt', 't']");
+  console.log("=>", consecutiveSubstrings('ttt'));
+
+  console.log("");
+
+  console.log("Test for numbers");
+  console.log("Expecting: ['1', '12', '123', '2', '23', '3']");
+  console.log("=>", consecutiveSubstrings('123'));
+
+  console.log("");
+
+  console.log("Test for special characters");
+  console.log("Expecting: ['!', '!@', '!@#', '@', '@#', '#']");
+  console.log("=>", consecutiveSubstrings('!@#'));
+
+  console.log("");
+
+  console.log("Test for mixed case");
+  console.log("Expecting: ['A', 'Ab', 'AbC', 'b', 'bC', 'C']");
+  console.log("=>", consecutiveSubstrings('AbC'));
+
+  console.log("");
+
+  console.log("Test for numbers and letters");
+  console.log("Expecting: ['a1c', 'a1', 'a1c', '1', '1c', 'c']");
+  console.log("=>", consecutiveSubstrings('a1c'));
+
+  console.log("");
+
+  console.log("Test for empty string");
+  console.log("Expecting: []");
+  console.log("=>", consecutiveSubstrings(''));
+
+  console.log("");
+
+  //Given Tests
   console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
   console.log("=>", consecutiveSubstrings('abc'));
 
@@ -14,5 +61,3 @@ if (require.main === module) {
 }
 
 module.exports = consecutiveSubstrings;
-
-
